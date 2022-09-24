13b1b053ccb567edb909b92eedd77d168ec9c881
MarcusFriberg <marcusfriberg78@icloud.com>
2022-09-24 12:08:09 +0200

    Initial commit

---
+++ src/test/java/com/grupp4/edufy/apigatewayservice/ApiGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupp4.edufy.apigatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
